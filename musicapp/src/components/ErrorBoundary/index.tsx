f7093df7a7d34245a4f4fef796ffe985a059c827
gokulmathew <gokul.srinivasan22@gmail.com>
2022-01-15 21:05:15 -0500

    1

--- musicapp/src/components/ErrorBoundary/index.tsx
+++ musicapp/src/components/ErrorBoundary/index.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class ErrorBoundary extends React.Component {
-  constructor(props) {
+  constructor(props: any) {
     super(props);
     this.state = { hasError: false };
   }
-
-  static getDerivedStateFromError(error) {
+  state: any = null;
+  static getDerivedStateFromError(error: any) {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch(error: any, errorInfo: any) {
     // You can also log the error to an error reporting service
     console.log(error, errorInfo);
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state && this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1 style={{textAlign:'center'}}>Something went wrong.</h1>;
+      return <h1 style={{ textAlign: "center" }}>Something went wrong.</h1>;
     }
 
     return this.props.children;
